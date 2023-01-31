2e24f0044ac0e8a160d2c98cd88459ec724c194b
bermeta <bermetzarlyk@gmail.com>
2023-01-31 21:24:47 +0600

    added orders and orderitems

--- rating/serializers.py
+++ rating/serializers.py
@@ -2,9 +2,18 @@ from rest_framework import serializers
 
 from rating.models import Review
 
+class ReviewActionSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.email')
+    product = serializers.ReadOnlyField(source='product.title')
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 
 class ReviewSerializers(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.email')
+
 
     class Meta:
         model = Review
@@ -17,6 +26,7 @@ class ReviewSerializers(serializers.ModelSerializer):
         if user.reviews.filter(product=product).exists():
             raise serializers.ValidationError('You already reviewed this product')
         return attrs
+
 
 class ReviewUpdateSerializers(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.email')
